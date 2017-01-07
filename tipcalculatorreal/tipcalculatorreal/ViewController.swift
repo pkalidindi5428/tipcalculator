f28470a79175352af64036240d5d8be9c0fccad8
pkalidindi5428 <pkalidindi5428@utexas.edu>
2017-01-06 23:53:06 -0600

    Initial Commit

--- tipcalculatorreal/tipcalculatorreal/ViewController.swift
+++ tipcalculatorreal/tipcalculatorreal/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
@@ -28,9 +29,9 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     @IBAction func calculateTip(_ sender: Any) {
-        
+        let tipPercentages = [0.18, 0.2, 0.25]
         let bill = Double(billField.text!) ?? 0
-        let tip = bill * 0.2
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
